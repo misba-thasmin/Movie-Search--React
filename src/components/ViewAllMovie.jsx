6abd845792d6e8f514b92d1bc50748b042212570
Misba Thasmin <misba-thasmin>
2025-07-10 18:46:35 +0530

    project created

--- src/components/ViewAllMovie.jsx
+++ src/components/ViewAllMovie.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import AddMovie from './AddMovie'
+import MovieHeader from './MovieHeader'
 
 const ViewAllMovie = () => {
   return (
     <div>
+      <MovieHeader />
 <div className="container">
     <div className="row">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -90,10 +93,10 @@ const ViewAllMovie = () => {
                 <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
 <div class="card">
-  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQixmhXWcL1ybgo3aMBmkYks8rjxH8rmY4JhA&s" class="card-img-top" alt="..."></img>
+  <img src="https://m.media-amazon.com/images/M/MV5BOTI3ZDZjMzAtZTkyYi00YTc4LWEyNDctYzNlOTc2ODIwMWNhXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg" class="card-img-top" alt="..."></img>
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text"></p>
+    <h5 class="card-title">Planet Dune</h5>
+    <p class="card-text">310</p>
     <a href="#" class="btn btn-primary">Select</a>
   </div>
 </div>
@@ -101,10 +104,10 @@ const ViewAllMovie = () => {
                 <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
 <div class="card">
-  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQixmhXWcL1ybgo3aMBmkYks8rjxH8rmY4JhA&s" class="card-img-top" alt="..."></img>
+  <img src="https://upload.wikimedia.org/wikipedia/en/thumb/8/8e/Dune_%282021_film%29.jpg/250px-Dune_%282021_film%29.jpg" class="card-img-top" alt="..."></img>
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text"></p>
+    <h5 class="card-title">Dune</h5>
+    <p class="card-text">455</p>
     <a href="#" class="btn btn-primary">Select</a>
   </div>
 </div>
@@ -112,10 +115,10 @@ const ViewAllMovie = () => {
                 <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
 <div class="card">
-  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQixmhXWcL1ybgo3aMBmkYks8rjxH8rmY4JhA&s" class="card-img-top" alt="..."></img>
+  <img src="https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg" class="card-img-top" alt="..."></img>
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text"></p>
+    <h5 class="card-title">Interstellar</h5>
+    <p class="card-text">250</p>
     <a href="#" class="btn btn-primary">Select</a>
   </div>
 </div>
